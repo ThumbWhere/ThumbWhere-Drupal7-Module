57484a7d44e491f15e55cc5d7c77a3b850e555c3
JamesMcParlane <james@massive.com.au>
2011-12-07 00:17:18 +1100

    New strategy

--- build/deploy_module_release.bat
+++ build/deploy_module_release.bat
@@ -2,31 +2,25 @@ REM Set the path to home where we have the config
 SET HOME=\home
 
 @REM Change to the release folder
-PUSHD ..\..\ThumbWhere-Drupal7-Module-Releases\release-history\
+PUSHD E:\checkout\ThumbWhere-Drupal7-Module-Releases\release-history\
+
+REM -----------------------------------
+REM Build package
+REM -----------------------------------
 
 @REM Make sure we have the feed (it will fail if it already exists - can be overidden by --force).
 
-..\..\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe new ThumbWhere ThumbWhere 7.x http://drupalmodules.thumbwhere.com/release-history/ --exitcleanonerror
+E:\inetpub\wwwroot\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe new ThumbWhere ThumbWhere 7.x http://drupalmodules.thumbwhere.com/release-history/ --exitcleanonerror
 
 @REM Update the release
-..\..\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe add ..\..\ThumbWhere-Drupal7-Module\ThumbWhere ThumbWhere 7.x patch "Bug Fixes" dev %1 --recommended --supported --default
+E:\inetpub\wwwroot\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe add E:\inetpub\wwwroot\ThumbWhere-Drupal7-Module\ThumbWhere ThumbWhere 7.x patch "Bug Fixes" dev %1 --recommended --supported --default
 
-
+REM -----------------------------------
 REM Now we update the local respository
-PUSHD E:\checkout\ThumbWhere-Drupal7-Module-Releases
+REM -----------------------------------
 
 REM Make sure we are up to date
 "C:\Program Files\Git\bin\git.exe" pull
-POPD
-
-@REM Copy changes to our local copy of the repository
-xcopy /ERVY . E:\checkout\ThumbWhere-Drupal7-Module-Releases\release-history\
-
-POPD
-
-@REM Commit them
-
-PUSHD E:\checkout\ThumbWhere-Drupal7-Module-Releases
 
 REM Add the new changes
 "C:\Program Files\Git\bin\git.exe" add .
