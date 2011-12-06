d6eef664e1b51383c1de496acc6047ca5a7e28df
James McParlane <james@massive.com.au>
2011-12-06 18:15:28 +1100

    encoding issue?

--- build/deploy_module_release.bat
+++ build/deploy_module_release.bat
@@ -1,18 +1,26 @@
 @REM Change to the release folder
 PUSHD ..\..\ThumbWhere-Drupal7-Module-Releases\release-history\
+
 @REM Make sure we have the feed (it will fail if it already exists - can be overidden by --force).
+
 ..\..\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe new ThumbWhere ThumbWhere 7.x http://drupalmodules.thumbwhere.com/release-history/ --exitcleanonerror
+
 @REM Update the release
 ..\..\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe add ..\..\ThumbWhere-Drupal7-Module\ThumbWhere ThumbWhere 7.x patch "Bug Fixes" dev %1 --recommended --supported --default
 
 @REM Copy changes to our local copy of the repository
 xcopy /ERVY . E:\checkout\ThumbWhere-Drupal7-Module-Releases\release-history\
+
 POPD
 
 @REM Commit them
+
 PUSHD E:\checkout\ThumbWhere-Drupal7-Module-Releases
+
 REM Push the new changes
 git commit -a -m "New changes"
+
 REM Push them to the server
 git push
+
 POPD
