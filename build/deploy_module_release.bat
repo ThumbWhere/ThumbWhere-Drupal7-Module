b7000e514fa252eb0476cbd6d465d890ce5b32e4
James McParlane <james@massive.com.au>
2011-12-08 19:21:46 +1100

    Added error handling.

--- build/deploy_module_release.bat
+++ build/deploy_module_release.bat
@@ -12,12 +12,15 @@ PUSHD ..\..\ThumbWhere-Drupal7-Module-Releases\release-history\
 
 @REM Make sure we have the feed (it will fail if it already exists - can be overidden by --force).
 ..\..\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe new ThumbWhere ThumbWhere 7.x http://drupalmodules.thumbwhere.com/release-history/ --exitcleanonerror
+IF NOT ERRORLEVEL 0 GOTO ReportError
 
 @REM Update the release
 ..\..\ThumbWhere-Drupal7-Module\tools\DrupalUtil.exe add ..\..\ThumbWhere-Drupal7-Module\ThumbWhere ThumbWhere 7.x patch %MESSAGE% %STREAM% %BUILD% %4 %5 %6
+IF NOT ERRORLEVEL 0 GOTO ReportError
 
 @REM Copy changes to our local copy of the repository
 xcopy /ERVY . E:\checkout\ThumbWhere-Drupal7-Module-Releases\release-history\
+IF NOT ERRORLEVEL 0 GOTO ReportError
 
 POPD
 
@@ -30,14 +33,36 @@ SET HOME=\home
 
 REM Make sure we are up to date
 "C:\Program Files\Git\bin\git.exe" pull
+IF NOT ERRORLEVEL 0 GOTO ReportError
 
 REM Add the new changes
 "C:\Program Files\Git\bin\git.exe" add .
+IF NOT ERRORLEVEL 0 GOTO ReportError
 
 REM Add the new changes
 "C:\Program Files\Git\bin\git.exe" commit -m "Automatic commit by %STREAM% build %1."
+IF NOT ERRORLEVEL 0 GOTO ReportError
 
 REM Push the new changes
 "C:\Program Files\Git\bin\git.exe" push
+IF NOT ERRORLEVEL 0 GOTO ReportError
 
 POPD
+
+@goto ReportOK
+:ReportError
+@echo Project error: A tool returned an error code from the build event
+goto ReportNotOK
+:ReportOK
+@echo Completed package without errors
+goto Done
+:ReportKindOfOK
+@echo Completed package with errors that we are pretending didn't happen.
+goto Done
+:ReportNotOK
+@echo Completed package with errors.
+@exit 1
+:RollBack
+@echo Rollback not possible for this
+:Done
+
