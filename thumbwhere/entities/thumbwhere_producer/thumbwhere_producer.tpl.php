ae31819882c15fd2c7c1083c63a8a646643021f8
James McParlane <james@massive.com.au>
2012-02-23 12:23:07 +1100

    Renamed producer to thumbwhere_producer

--- thumbwhere/entities/thumbwhere_producer/thumbwhere_producer.tpl.php
+++ thumbwhere/entities/thumbwhere_producer/thumbwhere_producer.tpl.php
@@ -2,22 +2,22 @@
 
 /**
  * @file
- * A basic template for producer entities
+ * A basic template for thumbwhere_producer entities
  *
  * Available variables:
  * - $content: An array of comment items. Use render($content) to print them all, or
  *   print a subset such as render($content['field_example']). Use
  *   hide($content['field_example']) to temporarily suppress the printing of a
  *   given element.
- * - $title: The name of the producer
- * - $url: The standard URL for viewing a producer entity
+ * - $title: The name of the thumbwhere_producer
+ * - $url: The standard URL for viewing a thumbwhere_producer entity
  * - $page: TRUE if this is the main view page $url points too.
  * - $classes: String of classes that can be used to style contextually through
  *   CSS. It can be manipulated through the variable $classes_array from
  *   preprocess functions. By default the following classes are available, where
  *   the parts enclosed by {} are replaced by the appropriate values:
  *   - entity-profile
- *   - producer-{TYPE}
+ *   - thumbwhere_producer-{TYPE}
  *
  * Other variables:
  * - $classes_array: Array of html class attribute values. It is flattened
