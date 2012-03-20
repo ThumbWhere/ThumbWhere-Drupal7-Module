139a8c1a4d35b1cfa0e18df5cfd1e23be12573c1
James McParlane <james@massive.com.au>
2012-03-20 23:10:10 +1100

    More generated code.

--- thumbwhere/entities/update.bat
+++ thumbwhere/entities/update.bat
@@ -1,4 +1,6 @@
 PUSHD \Inetpub\wwwroot\3520_ThumbWhereMediaServer
 CALL autogen_model_implementation_drupal.bat
+CALL autogen_services_client_code.bat
 POPD
 xcopy /ERVY ..\..\..\3520_ThumbWhereMediaServer\integrations\drupal\thumbwhere\entities .
+xcopy /ERVY ..\..\..\3520_ThumbWhereMediaServer\integrations\drupal\thumbwhere\dependencies\tw\services ..\dependencies\tw\services\
